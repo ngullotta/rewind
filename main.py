7fd5d992199c1085866f9344add8f422bc648ea4
Nikolaus Gullotta <nikolaus.gullotta@protonmail.com>
2021-03-06 09:59:52 -0600

    Patch setup_streamlink method in streamlink_cli.main

---
+++ main.py
@@ -0,0 +1,13 @@
+from streamlink_cli import main
+from streamlink_cli.console import ConsoleUserInputRequester
+
+
+from rewind.client import Client
+
+
+def patched_streamlink_setup():
+    cir = ConsoleUserInputRequester(main.console)
+    main.streamlink = Client({"user-input-requester": cir})
+
+main.setup_streamlink = patched_streamlink_setup
+main.main()
