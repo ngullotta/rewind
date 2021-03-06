c320574a26bf21e75853ab21afc92feca8e36cb8
Nikolaus Gullotta <nikolaus.gullotta@protonmail.com>
2021-03-06 09:59:34 -0600

    Auto import rewind imports into Client

--- rewind/client.py
+++ rewind/client.py
@@ -1,8 +1,19 @@
 from streamlink import Streamlink
 from streamlink.plugin import Plugin
 
+from rewind.plugins import TwitchRewind
+
 
 class Client(Streamlink):
+
+    REWIND_OVERWRITES = {
+        "twitch": TwitchRewind
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, plugin in self.REWIND_OVERWRITES.items():
+            self.add_plugin(plugin, name)
 
     def add_plugin(self, plugin: Plugin, name: str = "unknown") -> Plugin:
         plugin.bind(self, name, self.get_option("user-input-requester"))
