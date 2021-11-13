52cd4141a5d08864c23d4ea1317708d6fdb5288a
Nikolaus Gullotta <nikolaus.gullotta@protonmail.com>
2021-11-12 22:01:51 -0600

    Better GQL call, getting closer

--- rewind/plugins/twitch.py
+++ rewind/plugins/twitch.py
@@ -1,24 +1,32 @@
 import logging
+from datetime import timedelta
 from typing import OrderedDict, Union
 
 from streamlink.plugin import PluginArgument, PluginArguments
+from streamlink.plugin.api import validate
 from streamlink.plugins.twitch import Twitch, TwitchAPI
 
 StreamReturnType = Union[OrderedDict, None]
 
 
 class TwitchAPIRewind(TwitchAPI):
-    def video_tower(self, channel):
+    def video_tower(self, channel: str, limit: int = 30) -> dict:
         query = self._gql_persisted_query(
             "FilterableVideoTower_Videos",
             "a937f1d22e269e39a03b509f65a7490f9fc247d7f83d6ac1421523e3b68042cb",
             broadcastType="ARCHIVE",
             channelOwnerLogin=channel,
-            limit=30,
+            limit=limit,
             videoSort="TIME",
         )
 
-        return self.call(query)
+        schema = validate.Schema(
+            {"data": {"user": {"videos": {"edges": list}}}},
+            validate.get(("data", "user", "videos", "edges"), []),
+            validate.map(lambda edge: edge["node"]),
+        )
+
+        return self.call(query, schema=schema)
 
 
 class TwitchRewind(Twitch):
@@ -61,9 +69,7 @@ class TwitchRewind(Twitch):
         streams = super()._get_streams()
         if streams is not None:
             return streams
-        return self._check_past_broadcasts()
 
-    def _check_past_broadcasts(self) -> StreamReturnType:
         self.logger.info(
             f"Channel {self.channel} is not currently streaming..."
         )
@@ -71,21 +77,26 @@ class TwitchRewind(Twitch):
         if not self.options.get("check_vods"):
             return None
 
-        self.logger.info("Checking for past broadcasts now")
+        return self._check_past_broadcasts()
 
-        rv = self.api.video_tower(self.channel)
-        data = rv.get("data", {})
-        user = data.get("user", {})
-        videos = user.get("videos", {})
-        edges = videos.get("edges", [])
-        for edge in edges:
-            node = edge.get("node", {})
+    def _check_past_broadcasts(self) -> StreamReturnType:
+        self.logger.info("Checking for past broadcasts...")
+
+        nodes = self.api.video_tower(self.channel)
+        for node in nodes:
             _id = node.get("id", "")
             title = node.get("title", "")
-            print(_id, "->", title)
+            print(
+                _id,
+                "->",
+                title,
+                timedelta(seconds=node.get("lengthSeconds")),
+                node.get("game", {}).get("name"),
+            )
             if _id:
                 self.video_id = _id
                 break
+
         return self._get_streams()
 
 
